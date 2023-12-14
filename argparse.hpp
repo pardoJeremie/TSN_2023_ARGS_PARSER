5548ddb312cacb3db214ed432518117bd121773f
pardoJeremie <pardo_jeremie@outlook.com>
2023-12-14 17:52:34 +0100

    Implemented all but the parser

--- argparse.hpp
+++ argparse.hpp
@@ -18,12 +18,20 @@
 #include <memory>
 #include <vector>
 #include <regex>
-
+#include <algorithm>
+#include <cctype>
+#include <type_traits>
+#include <tuple>
+#include <limits>
+#include <iomanip>
 
 namespace arg {
 
     template<class T>
     using ref = std::reference_wrapper<T>;
+    
+    template<class T>
+    using pairValInit = std::pair<arg::ref<T>,arg::ref<bool>>; // Contain the value and the initialized status of the arg of the option
 
     using var = std::variant<
         bool,
@@ -41,77 +49,63 @@ namespace arg {
          * Permet de mettre à jour "variant" avec "argv_str_value".
          * Convertir la chaine de charactere (argv_str_value) en entier si besoin.
          */
-        bool convert(const char *const argv_str_value, var& variant)
+        void convert(const char *const argv_str_value, var& variant) noexcept(false)
         {
-            bool converted = true;
             // Utiliser std::visit() et overload{}
             std::visit(overload {
-                [&](bool& a){converted = false;}, // should never be used
-                [&](int32_t& a){
-                    auto b = strtol(argv_str_value,NULL,10); // remarque If no valid conversion could be performed, a zero value is returned from strtol
-                    a = static_cast<int32_t>(b); },
-                [&](uint32_t& a){
+                [&](std::string& a){ a = argv_str_value;},
+                [&](auto& a){
+                    std::string s = argv_str_value;
+                    if(!std::any_of(s.begin(),s.end(),::isdigit))
+                        throw std::runtime_error("input value don't contain a digit!");
+                    
                     auto b = strtol(argv_str_value,NULL,10);
-                    if (b < 0)
-                        converted = false;
-                    else
-                        a = static_cast<uint32_t>(b);},
-                [&](int64_t& a){
-                    auto b = strtol(argv_str_value,NULL,10);
-                    a = static_cast<int32_t>(b); },
-                [&](uint64_t& a){
-                    auto b = strtol(argv_str_value,NULL,10);
-                    if (b < 0)
-                        converted = false;
-                    else
-                        a = static_cast<uint64_t>(b);},
-                [&](std::string& a){ a = argv_str_value;}}, variant);
-
-            return converted;
+                    
+                    if (std::is_same<bool&,decltype(a)>::value && (b != 1 || b != 0)) // attention, all possible type of the variable 'a' are reference!
+                        throw std::runtime_error("input value is not 0 or 1!");
+                    else if (std::is_unsigned<decltype(a)>::value && b < 0)
+                        throw std::runtime_error("input value is negative!");
+                    else if (true) //si la valeur est trop grande !!!!
+                        throw std::runtime_error("input value is is to big!");
+                
+                    a = static_cast< std::remove_reference_t<decltype(a)>>(b); // remove the reference of var&
+                    // remark: using the equivalent std::remove_reference<decltype(a)>::type create a "Missing 'typename' ... " error
+                }}, variant);
         }
 
         /**
          * Permet de convertir un variant en std::string.
          */
-        std::string to_string(const var& v)
+        std::string to_string(const var& v) noexcept
         {
             // Utiliser std::visit() et overload{}
             std::stringstream ss;
             
             std::visit(overload {
                 [&](const bool& a){ss<<(a?"true":"false");},
-                [&](const int32_t& a){ss<<a;},
-                [&](const uint32_t& a){ss<<a;},
-                [&](const int64_t& a){ss<<a;},
-                [&](const uint64_t& a){ss<<a;},
-                [&](const std::string& a){ss << '\"' << a << '\"';}}, v);
+                [&](const auto& a){ss<<a;}}, v);
             
             return ss.str();
+        }
+    
+        std::string varType_to_string(const var& v) noexcept // typeid(decltype(a)).name() don't return a satifiable name
+        {
+            std::string s;
+        
+            std::visit(overload {
+                [&](const bool& a){s="bool";},
+                [&](const int32_t& a){s="int32_t";},
+                [&](const uint32_t& a){s="uint32_t";},
+                [&](const int64_t& a){s="int64_t";},
+                [&](const uint64_t& a){s="uint64_t";},
+                [&](const std::string& a){s="string";}}, v);
+        
+            return s;
         }
     }
 
     class Options
     {
-    private:
-        /**
-         * Les differents nom de l'option
-         */
-        struct ts_ids final {
-            std::string id{};   // nom long (ex: "opt1")
-            char alias = '\0';  // alias (ex: 'i')
-        };
-
-        /**
-         * Définition d'une option
-         */
-        struct ts_option final {
-            ts_ids ids{};       // nom de l'option
-            var value;          // valeur de l'option
-            std::string help{}; // description de l'option
-        };
-        
-        std::vector<std::unique_ptr<ts_option>> m_options{};
-
     public:
 
         /**
@@ -127,40 +121,36 @@ namespace arg {
         
         
         template<typename T>
-        ref<T> op(const std::string& name, const std::string& help, const std::optional<T>& opt = std::nullopt)
+        pairValInit<T> op(const std::string& name, const std::string& help, const std::optional<T>& opt = std::nullopt) noexcept(false)
         {
             T val;
+            bool as_default_Val = (opt != std::nullopt);
             
-            if(opt != std::nullopt)
+            if(as_default_Val)
                 val = opt.value();
+            else if (std::is_same<bool,T>::value) { // init bool opt value if no value is set;
+                val = false;
+                as_default_Val = true;
+            }
             
             if (!(name.size() > 2 && std::regex_match(name,std::regex("[a-zA-Z],[a-zA-Z0-9]*"))))
-                throw; // trow exception since the name is wrong
+                throw std::runtime_error("option have wrong alias and name format! the format should be \"[a-zA-Z],[a-zA-Z0-9]*\" ");
             char alias = name.front();
             std::string option_name = name;
-            option_name.erase(0,1);
+            option_name.erase(0,2);
             
             // Utiliser m_options.emplace_back() qui retourne une référence sur l'objet ajouté.
-            ref<T> op_val_ref = std::ref(std::get<T>(
-                m_options.emplace_back(
-                    std::make_unique<ts_option>(
-                        ts_option{ts_ids{name,alias}, val, help})).get()->value));
+            auto& opt_ref = m_options.emplace_back(std::make_unique<ts_option>( ts_option{ts_ids{option_name,alias}, val, help, as_default_Val}));
+
+            pairValInit<T> op_val_ref = { std::ref(std::get<T>(opt_ref.get()->value)), std::ref<bool>(opt_ref.get()->initialized)};
             
             return op_val_ref;
         }
 
         /**
-         * Affiche les options avec leurs valeurs par defaut.
-         */
-        void help()
-        {
-
-        }
-
-        /**
          * Parse les arguments du programme (argv).
          */
-        void parse(int argc, const char *const *argv)
+        void parse(int argc, const char *const *argv) // noexcept ou noexcept(false)
         {
             // Parcourir "m_options" pour mettre à jour les valeurs en fonction de "argv".
             for(;false;) {
@@ -169,11 +159,56 @@ namespace arg {
         }
 
         template<typename T>
-        static T parse(int argc, const char *const *argv) {
+        static T parse(int argc, const char *const *argv) noexcept {
             T args{};
             args.parse(argc, argv);
             return args;
         }
+
+        private:
+        
+        /**
+         * Affiche les options avec leurs valeurs par defaut.
+         */
+        void help() noexcept
+        {
+            std::stringstream ss;
+            
+            ss << "Usage:\n\t-h,--help\n";
+            
+            for(std::vector<std::unique_ptr<ts_option>>::iterator it = m_options.begin(); it != m_options.end(); ++it) {
+                ss << "\t-" << it->get()->ids.alias << ",--" << it->get()->ids.id << " : " << details::varType_to_string(it->get()->value);
+                
+                if(it->get()->initialized) // Since the fonction can only be called via '-h,--help', 'initialized == true' implies that the variant have a default value.
+                    ss << " (" << details::to_string(it->get()->value) <<")";
+                
+                ss <<"\n";
+            }
+            
+            std::cout << ss.str() << std::endl;
+        }
+        
+        /**
+         * Les differents nom de l'option
+         */
+        struct ts_ids final {
+            std::string id{};   // nom long (ex: "opt1")
+            char alias = '\0';  // alias (ex: 'i')
+        };
+
+        /**
+         * Définition d'une option
+         */
+        struct ts_option final {
+            ts_ids ids{};       // nom de l'option
+            var value;          // valeur de l'option
+            std::string help{}; // description de l'option
+            bool initialized = false; // value as default or is initialized
+        };
+        
+        std::vector<std::unique_ptr<ts_option>> m_options{};
+
     };
+
 }
 #endif /* argparse_hpp */
--- main.cpp
+++ main.cpp
@@ -10,26 +10,27 @@
 
 struct MyOptions : public arg::Options
 {
-    arg::ref<int32_t> opt1 = op<int32_t>("i,opt1", "option 1 description", -1);
-    // default value -1
+    arg::pairValInit<int32_t> opt1 = op<int32_t>("i,opt1", "option 1 description", -1);
+    // default value = -1
     
-    arg::ref<uint64_t> opt2 = op<uint64_t>("u,opt2", "option 2 description");
+    arg::pairValInit<uint64_t> opt2 = op<uint64_t>("u,opt2", "option 2 description");
     // no default value, default constructed
     
-    arg::ref<bool> opt3 = op<bool>("b,opt3", "option 3 description");
+    arg::pairValInit<bool> opt3 = op<bool>("b,opt3", "option 3 description");
     // default false
     
-    arg::ref<std::string> opt4 = op<std::string>("s,opt4", "option 4 description","default value");
-    // default value "default value"
+    arg::pairValInit<std::string> opt4 = op<std::string>("s,opt4", "option 4 description","default value");
+    // default value = "default value"
 };
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     auto options = arg::Options::parse<MyOptions>(argc, argv);
-    std::cout << "result: args.opt1= " << options.opt1 << std::endl;
-    std::cout << "result: args.opt2= " << options.opt2 << std::endl;
-    std::cout << "result: args.opt3= " << options.opt3 << std::endl;
-    std::cout << "result: args.opt4= " << options.opt4.get() << std::endl;
+
+    std::cout << "result: args opt1 = " << (options.opt1.second? std::to_string(options.opt1.first): "?" )
+    << "\n        args opt2 = " << (options.opt2.second? std::to_string(options.opt2.first): "?" )
+    << "\n        args opt3 = " << (options.opt3.second? std::to_string(options.opt3.first): "?" )
+    << "\n        args opt4 = " << (options.opt4.second? options.opt4.first.get(): "?" ) << std::endl;
     
     return 0;
 }
